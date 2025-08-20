99fd305b8b197b644e57b9b3f8f660db5793b0bf
Alexpena76 <alexpenasilva@gmail.com>
2025-08-20 10:59:52 -0500

    Betty question

---
+++ shell.c
@@ -0,0 +1,67 @@
+#include "shell.h"
+
+/**
+ * main - Entry point for simple shell
+ * @argc: Argument count
+ * @argv: Argument vector
+ * 
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+
+}
+
+/**
+ * shell_loop - Main shell execution loop
+ * @program_name: Name of the shell program (argv[0])
+ * 
+ * Return: Void function
+ */
+
+void shell_loop(char *program_name)
+{
+
+}
+
+/**
+ * shell_loop - Main shell execution loop
+ * @program_name: Name of the shell program (argv[0])
+ * 
+ * Return: Void function
+ */
+
+void shell_loop(char *program_name)
+{
+
+}
+
+/**
+ * parse_input - Parse command line input into command structure
+ * @input: Raw input string from user
+ * 
+ * Return: Pointer to cmd_t structure, or NULL on error
+ */
+
+cmd_t *parse_input(char *input)
+{
+
+}
+
+/**
+ * split_string - Split string into array of tokens
+ * @str: String to split
+ * @delim: Delimiter characters
+ * 
+ * Return: Array of string tokens, or NULL on error
+ */
+
+char **split_string(char *str, char *delim)
+{
+
+}
+
+/**
+ * execute_command - Execute a parsed command
+ */
---
+++ shell.h
@@ -0,0 +1,45 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <signal.h>
+
+#define MAX_INPUT_SIZE 1024
+#define MAX_ARGS 64
+#define PROMPT "($) "
+
+extern char **environ;
+
+typedef struct {
+	char *command;
+	char **args;
+	int arg_count;
+} cmd_t;
+
+void shell_loop(char *program_name);
+void display_prompt(void);
+char *read_input(void);
+cmd_t *parse_input(char *input);
+int execute_command(cmd_t *cmd, char *program_name);
+
+char *find_command_in_path(char *command);
+int is_executable(char *path);
+
+int handle_builtin(cmd_t *cmd);
+int builtin_exit(cmd_t *cmd);
+int builtin_env(cmd_t *cmd);
+
+void print_error(char *program_name, char *command, char *message);
+
+void free_cmd(cmd_t *cmd);
+char **split_string(char *str, char *delim);
+void free_array(char **array);
+
+#endif
