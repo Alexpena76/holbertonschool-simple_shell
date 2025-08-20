3dab4cbee22fac27f06d82121e3620938c631fc3
Alexpena76 <alexpenasilva@gmail.com>
2025-08-20 11:01:40 -0500

    Betty question corrections

--- shell.c
+++ shell.c
@@ -4,7 +4,7 @@
  * main - Entry point for simple shell
  * @argc: Argument count
  * @argv: Argument vector
- * 
+ *
  * Return: Always 0 (Success)
  */
 
@@ -16,7 +16,7 @@ int main(int argc, char *argv[])
 /**
  * shell_loop - Main shell execution loop
  * @program_name: Name of the shell program (argv[0])
- * 
+ *
  * Return: Void function
  */
 
@@ -28,7 +28,7 @@ void shell_loop(char *program_name)
 /**
  * shell_loop - Main shell execution loop
  * @program_name: Name of the shell program (argv[0])
- * 
+ *
  * Return: Void function
  */
 
@@ -40,7 +40,7 @@ void shell_loop(char *program_name)
 /**
  * parse_input - Parse command line input into command structure
  * @input: Raw input string from user
- * 
+ *
  * Return: Pointer to cmd_t structure, or NULL on error
  */
 
@@ -53,7 +53,7 @@ cmd_t *parse_input(char *input)
  * split_string - Split string into array of tokens
  * @str: String to split
  * @delim: Delimiter characters
- * 
+ *
  * Return: Array of string tokens, or NULL on error
  */
 
